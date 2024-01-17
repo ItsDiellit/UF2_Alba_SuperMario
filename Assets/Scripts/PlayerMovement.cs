15579aa638ca99a31c80841e2d6667ba40ce3d92
ItsDiellit <albadsdg@gmail.com>
2024-01-17 10:39:55 +0100

    movimiento y salto commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D rBody;
+    public GroundSensor sensor;
+
+    // Para asignar Mario a rigidbody2D a traves de privada 
     public Vector3 newPosition = new Vector3(50,5,0);
 
     public float movementSpeed = 5;
 
     private float inputHorizontal;
 
+    public float jumpForce = 10;
+
     public bool jump = false;
+
+    void Awake()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+        //Con GetComponent asignamos la variable "Rigidbody2D" al componente en cuestion
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -26,9 +38,11 @@ public class PlayerMovement : MonoBehaviour
     {
         inputHorizontal = Input.GetAxis("Horizontal");
         //transform.position = transform.position + new Vector3(1,0,0) * movementSpeed * Time.deltaTime;
-        transform.position += new Vector3(inputHorizontal,0,0) * movementSpeed * Time.deltaTime;
+        //transform.position += new Vector3(inputHorizontal,0,0) * movementSpeed * Time.deltaTime;
 
-        if(jump == true)
+        
+
+       /* if(jump == true)
         {
             Debug.Log("estoy saltando");
         }
@@ -39,6 +53,20 @@ public class PlayerMovement : MonoBehaviour
         else
         {
             Debug.Log("afsdg");
+        }*/
+//Getbutton Down para cuando pulsas el boton getbuttonup cuando sueltas y getbutton cuando mantienes la tecla
+        if(Input.GetButtonDown("Jump") && sensor.isGrounded == true )
+        { 
+            
+             rBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                //Si ponemos exclamacion en if(!...) le decimos que se active si el resultado es el contrario
         }
+    }
+
+    void FixedUpdate ()
+    {
+
+        rBody.velocity = new Vector2(inputHorizontal * movementSpeed , rBody.velocity.y);
+
     }
 }
